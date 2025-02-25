dc77e4f1d27711b4d338f528487ba6ba2237b4b9
Niki Nakamura <niki.nakamu@gmail.com>
2025-02-25 10:49:15 +0900

    Update insert_links.py

--- scripts/insert_links.py
+++ scripts/insert_links.py
@@ -102,11 +102,6 @@ def main():
     for kw, usage_info in link_usage.items():
         link_url = usage_info.get("url", "")
         for art_id in usage_info.get("articles_used_in", {}).keys():
-            article_to_kws.setdefault(art_id, {})[kw] = link_url
-
-     article_to_kws = {
-         "5187": article_to_kws.get("5187", {})
-     }
     
     for art_id, kw_map in article_to_kws.items():
         post_id = int(art_id)
